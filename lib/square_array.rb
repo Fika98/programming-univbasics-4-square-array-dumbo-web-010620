7d318f92cdf0372eaae0730ef1260da8555ab7a7
Firuz Afzalov <afzalov98@gmail.com>
2019-12-31 06:17:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,10 @@
 def square_array(array)
- array.each do |integer|
-  new_array = [integer ** 2]
- end
- return new_array
+counter = 0 
+
+while array[counter] do 
+  puts array[counter] ** 2
+  counter += 1
+end
 end
 
 square_array([1,2,3])
